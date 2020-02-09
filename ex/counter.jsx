cc661147d7c37024fe13a7f50800388ad86f4a6e
Marcio Silva <marcioadsworks@gmail.com>
2020-02-09 19:48:05 -0300

    counter with redux

--- ex/counter.jsx
+++ ex/counter.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { inc, dec, stepChanged } from './counterActions'
 
 const Counter = props => (
-    <div>Ok</div>
+    <div>
+        <h1>{props.counter.number}</h1>
+        <input onChange={props.stepChanged} value={props.counter.step} type='number'/>
+        <button onClick={props.inc}>INC</button>
+        <button onClick={props.dec}>DEC</button>
+    </div>
 )
 
-export default Counter
+const mapStateToProps = state =>({counter:state.counter})
+const mapDispatchToProps = dispatch => bindActionCreators({inc,dec,stepChanged},dispatch)
+export default connect(mapStateToProps,mapDispatchToProps)(Counter)
--- ex/index.jsx
+++ ex/index.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import { combineReducers, createStore } from 'redux'
 import { Provider } from 'react-redux'
-
+import counterReducers from './counterReducers'
+import Counter from './counter'
 
 
 const reducers = combineReducers({
-    field: fieldReducers
+    counter: counterReducers
 })
 
 ReactDom.render(
     <Provider store={createStore(reducers)}>
-        <Field initialValue="Digite" />
+      <Counter />
     </Provider>
     , document.getElementById('app'))
